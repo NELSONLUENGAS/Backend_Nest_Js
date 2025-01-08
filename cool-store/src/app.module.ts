2bd91ec695036509ca0569b0a865fa4431b1f6d2
NELSON ANDRES LUENGAS SILVA <90053867+NELSONLUENGAS@users.noreply.github.com>
2025-01-07 21:41:07 -0500

    update

--- cool-store/src/app.module.ts
+++ cool-store/src/app.module.ts
@@ -24,7 +24,7 @@ import config from './config';
                 DB_NAME: Joi.string().required(),
                 DB_USER: Joi.string().required(),
                 DB_PASSWORD: Joi.string().required(),
-                DB_PORT: Joi.number().required(),
+                DB_PORT: Joi.number().optional(),
                 DB_HOST: Joi.string().required(),
                 DB_CONECCTION: Joi.string().required(),
                 JWT_SECRET: Joi.string().required(),
